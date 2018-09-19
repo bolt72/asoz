21e5a8e1559492c09f7e2363c6beae5936b6f2ae
Pavel Boltikov <boltikovpv@gmail.com>
2018-09-19 11:11:04 +1000

    TEST

---
+++ app.js
@@ -0,0 +1,97 @@
+var items = [
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+]
+
+var Tmap = Vue.component('tmap', 
+{
+	template: `
+		<b-table striped hover :items="items"></b-table>
+	`
+});
+
+var Asoz = Vue.component('asoz', 
+{
+	template: `
+		<b-card
+				tag="article"
+				class="mb-12"
+				style="height: 99%">
+
+			<b-button-toolbar aria-label="Toolbar with button groups and input groups">
+				<b-button-group size="sm" class="mx-1">
+					<b-btn>New</b-btn>
+					<b-btn>Edit</b-btn>
+				</b-button-group>
+				<b-input-group size="sm" class="w-25 mx-1" prepend="$" append=".00">
+					<b-form-input value="100" class="text-right"></b-form-input>
+				</b-input-group>
+				<b-input-group  size="sm" class="w-25 mx-1" prepend="Size">
+					<b-form-select value="Medium" :options="['Large','Medium','Small']"></b-form-select>
+				</b-input-group>
+				<b-button-group  size="sm" class="mx-1">
+					<b-btn>Save</b-btn>
+					<b-btn>Cancel</b-btn>
+				</b-button-group>
+			</b-button-toolbar>
+			<div class="scrolling-wrapper" style="height: 85%">
+			
+			</div>
+		</b-card>
+	`
+});
+
+const routes = [
+  { path: '/asoz', component: Asoz },
+  { path: '/tmap', component: Tmap }
+]
+
+// 3. Создаём экземпляр маршрутизатора и передаём маршруты в опции `routes`
+// Вы можете передавать и дополнительные опции, но пока не будем усложнять.
+const router = new VueRouter({
+  routes // сокращённая запись для `routes: routes`
+})
+
+new Vue(
+{
+	data: 
+	{
+		items: items
+	}
+	,el: "#app"
+	,router
+});
+
+
---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+		<title>ЦТС Инфр</title>
+		<link type="text/css" rel="stylesheet" href="//unpkg.com/bootstrap/dist/css/bootstrap.min.css"/>
+		<link type="text/css" rel="stylesheet" href="//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.css"/>	</head>
+		<style>
+			.scrolling-wrapper 
+			{
+				overflow-x: hidden;
+				overflow-y: auto;
+				white-space: nowrap;
+			}
+		</style>
+	<body>
+		<div id="app">
+			<b-navbar toggleable="md" variant="primary" type="dark" variant="info" position="fixed">
+				<b-navbar-brand href="#">ЦТС Инфр</b-navbar-brand>
+				<b-navbar-nav>
+					<b-nav-item to="/asoz">АСОЗ</b-nav-item>
+					<b-nav-item to="/tmap">Часовые пояса</b-nav-item>
+				</b-navbar-nav>
+			</b-navbar>
+			<div class="scrolling-wrapper" style="height: 85%">
+				<router-view></router-view>
+			</div>
+		</div>
+		
+		<script src="https://cdn.jsdelivr.net/npm/vue"></script>
+		<script src="//unpkg.com/babel-polyfill@latest/dist/polyfill.min.js"></script>
+		<script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
+		<script src="//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.js"></script>
+		<script src="app.js"></script>
+	</body>
+</html>
+
